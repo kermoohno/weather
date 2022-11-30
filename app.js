ccbcf3a5f2d3606deaa26b6387ffd67d4fe831a5
Kermo <kermo.ohno@voco.com>
2022-11-30 10:46:36 +0200

    Displayed city weather data from API

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+document.addEventListener('DOMContentLoaded', cityWeather)
+
+
 function weatherDataFetch( city ) {
     var key = 'd4704927e8828d83ba4cc33b4483e308'
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
@@ -14,7 +17,14 @@ function weatherDataFetch( city ) {
 }
 
 function cityWeather(e) {
-    weatherDataFetch( 'Tallinn' )
+    weatherDataFetch( 'Radeberg' );
 }
 
-console.log(cityWeather())
+function drawWeather( data ) {
+    var celcius = Math.round(parseFloat(data.main.temp)-273.15);
+    var description = data.weather[0].description;
+
+    document.querySelector('#description').innerHTML = description;
+    document.querySelector('#temp').innerHTML = celcius + '&deg;';
+    document.querySelector('#location').innerHTML = data.name;
+}
